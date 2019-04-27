b02d9ab0b5bf703f964466b5375c397af714ceb8
DeMario <demario.windom@gmail.com>
2019-04-27 00:55:04 -0400

    Tail Wind CSS

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_SERVER", ($_SERVER['REMOTE_ADDR'] === "127.0.0.1" ) ? "localhost" : "remote");
+define("DB_SERVER", ($_SERVER['REMOTE_ADDR'] === "::1" ) ? "localhost" : "remote");
 define("DB_PORT", (DB_SERVER ==="localhost") ? 5432 : 0000);
 define("DB_USER" , (DB_SERVER ==="localhost") ? "postgres" : "dwindom");
 define("DB_PASS", (DB_SERVER ==="localhost") ? "test123" : "");
